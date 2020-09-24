8a5e7826d8eb7a71b91593534e08b8dd952fcf06
iznauy <161250220@smail.nju.edu.cn>
2020-09-24 12:43:52 +0800

    init project

---
+++ bigfile-server/src/main/java/cn/edu/tsinghua/bigfileserver/BigfileServerApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.tsinghua.bigfileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigfileServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigfileServerApplication.class, args);
+    }
+
+}
---
+++ bigfile-server/src/test/java/cn/edu/tsinghua/bigfileserver/BigfileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.tsinghua.bigfileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigfileServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
